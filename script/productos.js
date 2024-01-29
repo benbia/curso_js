bf4ee1d4e8f5d19de118d23bc6235a5686e4092b
lubentancor <bentancorlucia@gmail.com>
2024-01-29 19:46:01 -0300

    second commit

---
+++ script/productos.js
@@ -0,0 +1,12 @@
+const productos = [
+    {id: 1, marca:"Apple", nombre: "IPhone 15", precio: 1351},
+    {id: 2, marca:"Apple", nombre: "IPhone 14", precio: 1149},
+    {id: 3, marca:"Apple", nombre: "IPhone 13", precio: 1049},
+    {id: 4, marca:"Samsung", nombre: "Galaxy S24", precio: 1300},
+    {id: 5, marca:"Samsung", nombre: "Galaxy A15", precio: 259},
+    {id: 6, marca:"Samsung", nombre: "Galaxy A54", precio: 448},
+    {id: 7, marca:"Apple", nombre: "Apple Watch Series 8", precio: 679},
+    {id: 8, marca:"Apple", nombre: "Apple Watch Series 7", precio: 524},
+    {id: 9, marca:"Samsung", nombre: "Galaxy Fit 2", precio: 49},
+    {id: 10, marca:"Samsung", nombre: "Galaxy Watch 5", precio: 219}
+]
